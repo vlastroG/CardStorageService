89dc3abb533c8c71159c96122e4fbde33df24047
Vladimir Stroganov <vlastroganov@gmail.com>
2022-09-18 22:00:53 +0300

    Базовая реализация контроллеров Card и Client

--- CardStorageService/Controllers/ClientController.cs
+++ CardStorageService/Controllers/ClientController.cs
@@ -1,14 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CardStorageService.Data;
+using CardStorageService.Models.Requests;
+using CardStorageService.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CardStorageService.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class ClientController : Controller
     {
         private readonly ILogger<ClientController> _logger;
+        private readonly IClientRepositoryService _clientRepositoryService;
 
-        public ClientController(ILogger<ClientController> logger)
+        public ClientController(ILogger<ClientController> logger,
+            IClientRepositoryService clientRepositoryService)
         {
             _logger = logger;
+            _clientRepositoryService = clientRepositoryService;
+        }
+
+        [HttpPost("create")]
+        [ProducesResponseType(typeof(CreateClientResponse), StatusCodes.Status200OK)]
+        public IActionResult Create([FromBody] CreateClientRequest request)
+        {
+            try
+            {
+                var clientId = _clientRepositoryService.Create(new Client
+                {
+                    FirstName = request.FirstName,
+                    Surname = request.Surname,
+                    Patronymic = request.Patronymic
+                });
+                return Ok(new CreateClientResponse
+                {
+                    ClientId = clientId
+                });
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Create client error.");
+                return Ok(new CreateCardResponse
+                {
+                    ErrorCode = 912,
+                    ErrorMessage = "Create client error."
+                });
+            }
         }
     }
 }
