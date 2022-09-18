e756d70d2bd31c1027a7a0ad0b03d6a22da6fc5f
Vladimir Stroganov <vlastroganov@gmail.com>
2022-09-18 22:00:19 +0300

    Создать классы запросов и ответов для добавления Client и Card

---
+++ CardStorageService/Models/CardDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CardStorageService.Models
+{
+    public class CardDto
+    {
+        public string? CardN { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? CVV2 { get; set; }
+
+        public string ExpDate { get; set; }
+    }
+}
