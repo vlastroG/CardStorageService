96b279833ce4c4430c956f9e9e09aeb06967d86a
Vladimir Stroganov <vlastroganov@gmail.com>
2022-09-25 18:53:00 +0300

    Создать консольное приложение для работы с PasswordUtils

---
+++ PasswordUtilsClient/Program.cs
@@ -0,0 +1,6 @@
+﻿using CardStorageService.Utils;
+
+var res = PasswordUtils.CreatePasswordHash("qwerty");
+Console.WriteLine(res.passwordSalt);
+Console.WriteLine(res.passwordHash);
+Console.ReadKey(true);
