7c202ea073b1adff9b1cbec755ea8b28a522651d
Vladimir Stroganov <vlastroganov@gmail.com>
2022-09-25 18:28:34 +0300

    Создать тестовый проект JwtSample

---
+++ JwtSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using JwtSample;
+//https://jwt.io/
+while (true)
+{
+    Console.WriteLine("Enter user name: ");
+    string userName = Console.ReadLine();
+    if (userName == "exit") break;
+    Console.WriteLine("Enter user password: ");
+    string userPassword = Console.ReadLine();
+    UserService userService = new UserService();
+    string token = userService.Authenticate(userName, userPassword);
+    Console.WriteLine(token);
+    Console.WriteLine();
+    Console.ReadKey(true);
+}
---
+++ JwtSample/UserService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtSample
+{
+    internal class UserService
+    {
+        private const string SecretCode = "23^%#&gfdnb6!$53eSFGT";
+
+        private IDictionary<string, string> _users = new Dictionary<string, string>()
+        {
+            {"user1", "qwerty"}, // 0
+            {"user2", "qwerty"}, // 1
+            {"user3", "qwerty"}, // 2
+            {"user4", "qwerty"}  // 3
+        };
+
+        public string Authenticate(string user, string password)
+        {
+            if (string.IsNullOrWhiteSpace(user) ||
+                string.IsNullOrWhiteSpace(password))
+            {
+                return string.Empty;
+            }
+
+            int i = 0;
+            foreach (KeyValuePair<string, string> pair in _users)
+            {
+                if (string.CompareOrdinal(pair.Key, user) == 0 &&
+                string.CompareOrdinal(pair.Value, password) == 0)
+                {
+                    return GenerateJwtToken(i);
+                }
+                i++;
+            }
+            return String.Empty;
+        }
+
+        private string GenerateJwtToken(int id)
+        {
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            byte[] key = Encoding.ASCII.GetBytes(SecretCode);
+
+            SecurityTokenDescriptor securityTokenDescriptor = new SecurityTokenDescriptor();
+            securityTokenDescriptor.Expires = DateTime.UtcNow.AddMinutes(15);
+            securityTokenDescriptor.SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature);
+            securityTokenDescriptor.Subject = new System.Security.Claims.ClaimsIdentity(new Claim[] {
+                new Claim(ClaimTypes.NameIdentifier, id.ToString())
+            });
+
+            SecurityToken securityToken = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+            return jwtSecurityTokenHandler.WriteToken(securityToken);
+        }
+    }
+}
