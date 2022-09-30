d7512d87c11d837084d61398c1c6d61f85a2d81e
Vladimir Stroganov <vlastroganov@gmail.com>
2022-09-30 23:18:18 +0300

    Реализовать AuthenticationService

---
+++ CardStorageService/Models/SessionInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace CardStorageService.Models
+{
+    public class SessionInfo
+    {
+        public int SessionId { get; set; }
+
+        public string SessionToken { get; set; }
+
+        public AccountDto Account { get; set; }
+    }
+}
---
+++ CardStorageService/Services/IAuthenticateService.cs
@@ -0,0 +1,12 @@
+﻿using CardStorageService.Models;
+using CardStorageService.Models.Requests;
+
+namespace CardStorageService.Services
+{
+    public interface IAuthenticateService
+    {
+        AuthenticationResponse Login(AuthenticationRequest authenticationRequest);
+
+        public SessionInfo GetSessionInfo(string sessionToken);
+    }
+}
