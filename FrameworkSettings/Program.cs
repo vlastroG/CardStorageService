69783d4684fb8f24406fd1c0cee59ac12ecadba8
Vladimir Stroganov <vlastroganov@gmail.com>
2022-10-03 21:52:00 +0300

    Тест добавления настроек

---
+++ FrameworkSettings/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkSettings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            Console.Title = Properties.Settings.Default.ApplicationNameDebug;
+#else
+            Console.Title = Properties.Settings.Default.ApplicationName;
+#endif
+            var test = Properties.Settings.Default.ApplicationNameDebug;
+
+            if (String.IsNullOrEmpty(Properties.Settings.Default.UserFullName) ||
+                Properties.Settings.Default.UserAge <= 0)
+            {
+                Console.Write("Введите ФИО:\n-->");
+                Properties.Settings.Default.UserFullName = Console.ReadLine();
+
+                Console.Write("Введите свой возраст:\n-->");
+                if (int.TryParse(Console.ReadLine(), out int age))
+                {
+                    Properties.Settings.Default.UserAge = age;
+                }
+                else
+                {
+                    Properties.Settings.Default.UserAge = 0;
+                }
+                Properties.Settings.Default.Save();
+            }
+            Console.WriteLine($"ФИО: {Properties.Settings.Default.UserFullName}");
+            Console.WriteLine($"Возраст: {Properties.Settings.Default.UserAge}");
+
+            Console.ReadKey(true);
+        }
+    }
+}
