0c23734f17ca53dc2e4a11a76957e4ec41d4d25a
Vladimir Stroganov <vlastroganov@gmail.com>
2022-10-09 13:51:35 +0300

    Пример Singleton

---
+++ Patterns/Program.cs
@@ -0,0 +1,20 @@
+﻿using Patterns.Sample_1;
+
+namespace Patterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sample Singleton
+            var singletonInstance1 = SampleSingleton.Instance;
+            singletonInstance1.Count++;
+            var singletonInstance2 = SampleSingleton.Instance;
+            singletonInstance2.Count++;
+            Console.WriteLine(singletonInstance1.Count);
+            bool test = singletonInstance1 == singletonInstance2;
+
+            // Sample AbstractFabric
+        }
+    }
+}
