92d489e36710242009f832f3f05b6e78c8cd09f8
Vladimir Stroganov <vlastroganov@gmail.com>
2022-10-09 22:52:03 +0300

    Добавить клиента gRPC для CardStorageService

---
+++ CardStorageClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using Grpc.Net.Client;
+using System.Net.WebSockets;
+using static CardStorageServiceProtos.CardService;
+using static CardStorageServiceProtos.ClientService;
+
+namespace CardStorageClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketHttpHandler.Http2UnencryptedSupport", true);
+
+            //CardServiceClient
+
+            //ClientServiceClient 
+
+            using var channel = GrpcChannel.ForAddress("http://localhost:5001");
+
+            ClientServiceClient clientService = new ClientServiceClient(channel);
+
+            var createClientResponse = clientService.Create(new CardStorageServiceProtos.CreateClientRequest
+            {
+                FirstName = "Роман",
+                Surname = "Чехов",
+                Patronymic = "Николаевич"
+            });
+
+            Console.WriteLine($"Client {createClientResponse.ClientId} created successfully\n=============================");
+
+            CardServiceClient cardService = new CardServiceClient(channel);
+
+            var getByClientIdResponse = cardService.GetByClientId(new CardStorageServiceProtos.GetByClientIdRequest
+            {
+                ClientId = 1
+            });
+            foreach (var card in getByClientIdResponse.Cards)
+            {
+                Console.WriteLine($"{card.CardN}; {card.Name}; {card.CVV2}; {card.ExpDate}");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
