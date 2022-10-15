68ebc8e6d2cc5e1d047c9381374c22fb76a97c75
Vladimir Stroganov <vlastroganov@gmail.com>
2022-10-16 00:49:40 +0300

    Добавить улучшенный поиск "в лоб" и benchmark на него

---
+++ FullTextSearchSimpleBenchmark/Benchmarks/SearchBenchmarkV1.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using FullTextSearchSimpleBenchmark.Services.Implements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullTextSearchSimpleBenchmark.Benchmarks
+{
+    [MemoryDiagnoser]
+    [WarmupCount(1)]
+    [IterationCount(5)]
+    public class SearchBenchmarkV1
+    {
+        private readonly string[] _documentsSet;
+
+        public SearchBenchmarkV1()
+        {
+            _documentsSet = DocumentExtractor.DocumentsSet().Take(10000).ToArray();
+        }
+
+        [Benchmark]
+        public void SimpleSearch()
+        {
+            new SimpleSearcherV2().SearchV3("Monday", _documentsSet).ToArray();
+        }
+    }
+}
